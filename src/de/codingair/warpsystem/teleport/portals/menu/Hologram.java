1bd48c439f84c5a37e302110e57ae5a25a3386b2
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2018-04-16 18:33:42 +0200

    [WS-35] Finished

--- src/de/codingair/warpsystem/teleport/portals/menu/Hologram.java
+++ src/de/codingair/warpsystem/teleport/portals/menu/Hologram.java
@@ -1,5 +1,6 @@
 package de.codingair.warpsystem.teleport.portals.menu;
 
+import de.codingair.codingapi.player.gui.hotbar.ClickEvent;
 import de.codingair.codingapi.player.gui.hotbar.ClickType;
 import de.codingair.codingapi.player.gui.hotbar.HotbarGUI;
 import de.codingair.codingapi.player.gui.hotbar.ItemComponent;
@@ -11,6 +12,7 @@ import de.codingair.warpsystem.WarpSystem;
 import de.codingair.warpsystem.language.Example;
 import de.codingair.warpsystem.language.Lang;
 import de.codingair.warpsystem.teleport.portals.PortalEditor;
+import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -48,9 +50,66 @@ public class Hologram extends HotbarGUI {
             menu.getEditor().doAction(PortalEditor.Action.CHANGE_START_NAME, () -> updateDisplayName(getItem(4), "§7"+Lang.get("Start_Name")+": '" + menu.getEditor().getPortal().getStartName()+"§7'"));
         }));
 
-        setItem(5, new ItemComponent(new ItemBuilder(Material.NAME_TAG).setName("§7"+Lang.get("Goal_Name", new Example("ENG", "Goal-Name"), new Example("GER", "Ziel-Name"))+": '" + menu.getEditor().getPortal().getDestinationName()+"§7'").getItem(), (gui, ic, player, clickType) -> {
+        ItemBuilder builder = new ItemBuilder(Material.STAINED_CLAY);
+        builder.setName(ChatColor.GRAY + Lang.get("Status_Of_Start", new Example("ENG", "Status of Start-Holo: "), new Example("GER", "Status vom Start-Holo: ")) +
+                (menu.getEditor().getPortal().isStartHoloStatus() ? ChatColor.GREEN + Lang.get("Enabled", new Example("ENG", "Enabled"), new Example("GER", "Aktiviert")) :
+                        ChatColor.RED + Lang.get("Disabled", new Example("ENG", "Disabled"), new Example("GER", "Deaktiviert"))));
+
+        if(menu.getEditor().getPortal().isStartHoloStatus()) builder.setColor(DyeColor.LIME);
+        else builder.setColor(DyeColor.RED);
+
+        setItem(5, new ItemComponent(builder.getItem(), new ClickEvent() {
+            @Override
+            public void onClick(HotbarGUI gui, ItemComponent ic, Player player, ClickType clickType) {
+                menu.getEditor().getPortal().setStartHoloStatus(!menu.getEditor().getPortal().isStartHoloStatus());
+                menu.getEditor().getPortal().updateHolograms();
+
+                ItemBuilder builder = new ItemBuilder(Material.STAINED_CLAY);
+                builder.setName(ChatColor.GRAY + Lang.get("Status_Of_Start", new Example("ENG", "Status of Start-Holo: "), new Example("GER", "Status vom Start-Holo: ")) +
+                        (menu.getEditor().getPortal().isStartHoloStatus() ? ChatColor.GREEN + Lang.get("Enabled", new Example("ENG", "Enabled"), new Example("GER", "Aktiviert")) :
+                                ChatColor.RED + Lang.get("Disabled", new Example("ENG", "Disabled"), new Example("GER", "Deaktiviert"))));
+
+                if(menu.getEditor().getPortal().isStartHoloStatus()) builder.setColor(DyeColor.LIME);
+                else builder.setColor(DyeColor.RED);
+
+                setItem(5, new ItemComponent(builder.getItem(), this), false);
+                updateSingle(5);
+            }
+        }));
+
+        setItem(6, new ItemComponent(new ItemBuilder(Material.STAINED_GLASS_PANE).setColor(DyeColor.BLACK).setHideName(true).getItem()));
+
+        setItem(7, new ItemComponent(new ItemBuilder(Material.NAME_TAG).setName("§7"+Lang.get("Goal_Name", new Example("ENG", "Goal-Name"), new Example("GER", "Ziel-Name"))+": '" + menu.getEditor().getPortal().getDestinationName()+"§7'").getItem(), (gui, ic, player, clickType) -> {
             //Goal-Name
             menu.getEditor().doAction(PortalEditor.Action.CHANGE_DESTINATION_NAME, () -> updateDisplayName(getItem(5), "§7"+Lang.get("Goal_Name")+": '" + menu.getEditor().getPortal().getDestinationName()+"§7'"));
+        }));
+
+
+        builder = new ItemBuilder(Material.STAINED_CLAY);
+        builder.setName(ChatColor.GRAY + Lang.get("Status_Of_Destination", new Example("ENG", "Status of Destination-Holo: "), new Example("GER", "Status vom Destination-Holo: ")) +
+                (menu.getEditor().getPortal().isDestinationHoloStatus() ? ChatColor.GREEN + Lang.get("Enabled", new Example("ENG", "Enabled"), new Example("GER", "Aktiviert")) :
+                        ChatColor.RED + Lang.get("Disabled", new Example("ENG", "Disabled"), new Example("GER", "Deaktiviert"))));
+
+        if(menu.getEditor().getPortal().isDestinationHoloStatus()) builder.setColor(DyeColor.LIME);
+        else builder.setColor(DyeColor.RED);
+
+        setItem(8, new ItemComponent(builder.getItem(), new ClickEvent() {
+            @Override
+            public void onClick(HotbarGUI gui, ItemComponent ic, Player player, ClickType clickType) {
+                menu.getEditor().getPortal().setDestinationHoloStatus(!menu.getEditor().getPortal().isDestinationHoloStatus());
+                menu.getEditor().getPortal().updateHolograms();
+
+                ItemBuilder builder = new ItemBuilder(Material.STAINED_CLAY);
+                builder.setName(ChatColor.GRAY + Lang.get("Status_Of_Destination", new Example("ENG", "Status of Destination-Holo: "), new Example("GER", "Status vom Destination-Holo: ")) +
+                        (menu.getEditor().getPortal().isDestinationHoloStatus() ? ChatColor.GREEN + Lang.get("Enabled", new Example("ENG", "Enabled"), new Example("GER", "Aktiviert")) :
+                                ChatColor.RED + Lang.get("Disabled", new Example("ENG", "Disabled"), new Example("GER", "Deaktiviert"))));
+
+                if(menu.getEditor().getPortal().isDestinationHoloStatus()) builder.setColor(DyeColor.LIME);
+                else builder.setColor(DyeColor.RED);
+
+                setItem(8, new ItemComponent(builder.getItem(), this), false);
+                updateSingle(8);
+            }
         }));
     }
 }
