feb30c4835adbc4e2757776c4a4013748ce4c042
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2018-04-17 00:22:52 +0200

    [WS-46] Finished

--- src/de/codingair/warpsystem/commands/CWarp.java
+++ src/de/codingair/warpsystem/commands/CWarp.java
@@ -1,11 +1,14 @@
 package de.codingair.warpsystem.commands;
 
+import de.codingair.codingapi.API;
 import de.codingair.codingapi.server.commands.BaseComponent;
 import de.codingair.codingapi.server.commands.CommandBuilder;
 import de.codingair.codingapi.server.commands.CommandComponent;
 import de.codingair.codingapi.server.commands.MultiCommandComponent;
 import de.codingair.warpsystem.WarpSystem;
+import de.codingair.warpsystem.gui.affiliations.Category;
 import de.codingair.warpsystem.gui.affiliations.Warp;
+import de.codingair.warpsystem.gui.guis.GWarps;
 import de.codingair.warpsystem.language.Example;
 import de.codingair.warpsystem.language.Lang;
 import org.bukkit.command.CommandSender;
@@ -29,12 +32,21 @@ public class CWarp extends CommandBuilder {
 
             @Override
             public void unknownSubCommand(CommandSender sender, String label, String[] args) {
-
+                if(WarpSystem.getInstance().getFileManager().getFile("Config").getConfig().getBoolean("WarpSystem.Commands.Warp.GUI", false)) {
+                    CWarps.run(sender, null);
+                } else {
+                    sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_HELP", new Example("ENG", "&7Use: &e" + label + " <Warp-Name>"), new Example("GER", "&7Benutze: &e/" + label + " <Warp-Name>")));
+                }
             }
 
             @Override
             public boolean runCommand(CommandSender sender, String label, String[] args) {
-                sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_HELP", new Example("ENG", "&7Use: &e" + label + " <Warp-Name>"), new Example("GER", "&7Benutze: &e/" + label + " <Warp-Name>")));
+                if(WarpSystem.getInstance().getFileManager().getFile("Config").getConfig().getBoolean("WarpSystem.Commands.Warp.GUI", false)) {
+                    CWarps.run(sender, null);
+                } else {
+                    sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_HELP", new Example("ENG", "&7Use: &e" + label + " <Warp-Name>"), new Example("GER", "&7Benutze: &e/" + label + " <Warp-Name>")));
+                }
+
                 return false;
             }
         }.setOnlyPlayers(true), true);
@@ -48,32 +60,42 @@ public class CWarp extends CommandBuilder {
         getBaseComponent().addChild(new MultiCommandComponent() {
             @Override
             public void addArguments(CommandSender sender, List<String> suggestions) {
-                for(Warp warp : WarpSystem.getInstance().getIconManager().getWarps()) {
-                    if(!warp.getNameWithoutColor().isEmpty()) suggestions.add(warp.getNameWithoutColor().replace(" ", "_"));
+                if(WarpSystem.getInstance().getFileManager().getFile("Config").getConfig().getBoolean("WarpSystem.Commands.Warp.GUI", false)) {
+                    for(Category c : WarpSystem.getInstance().getIconManager().getCategories()) {
+                        suggestions.add(c.getNameWithoutColor());
+                    }
+                } else {
+                    for(Warp warp : WarpSystem.getInstance().getIconManager().getWarps()) {
+                        if(!warp.getNameWithoutColor().isEmpty()) suggestions.add(warp.getNameWithoutColor().replace(" ", "_"));
+                    }
                 }
             }
 
             @Override
             public boolean runCommand(CommandSender sender, String label, String argument, String[] args) {
-                if(args.length == 0 || argument == null || argument.isEmpty()) {
-                    sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_HELP", new Example("ENG", "&7Use: &e" + label + " <Warp-Name>"), new Example("GER", "&7Benutze: &e/" + label + " <Warp-Name>")));
-                    return false;
+                if(WarpSystem.getInstance().getFileManager().getFile("Config").getConfig().getBoolean("WarpSystem.Commands.Warp.GUI", false)) {
+                    CWarps.run(sender, WarpSystem.getInstance().getIconManager().getCategory(argument));
+                } else {
+                    if(args.length == 0 || argument == null || argument.isEmpty()) {
+                        sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_HELP", new Example("ENG", "&7Use: &e" + label + " <Warp-Name>"), new Example("GER", "&7Benutze: &e/" + label + " <Warp-Name>")));
+                        return false;
+                    }
+
+                    argument = argument.replace("_", " ");
+                    Warp warp = null;
+                    for(Warp w : WarpSystem.getInstance().getIconManager().getWarps()) {
+                        if(w.getNameWithoutColor().equalsIgnoreCase(argument)) warp = w;
+
+                        if(warp != null) break;
+                    }
+
+                    if(warp == null) {
+                        sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_DOES_NOT_EXISTS", new Example("ENG", "&cThis warp does not exist."), new Example("GER", "&cDieser Warp existiert nicht.")));
+                        return false;
+                    }
+
+                    WarpSystem.getInstance().getTeleportManager().teleport((Player) sender, warp);
                 }
-
-                argument = argument.replace("_", "");
-                Warp warp = null;
-                for(Warp w : WarpSystem.getInstance().getIconManager().getWarps()) {
-                    if(w.getNameWithoutColor().equalsIgnoreCase(argument)) warp = w;
-
-                    if(warp != null) break;
-                }
-
-                if(warp == null) {
-                    sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_DOES_NOT_EXISTS", new Example("ENG", "&cThis warp does not exist."), new Example("GER", "&cDieser Warp existiert nicht.")));
-                    return false;
-                }
-
-                WarpSystem.getInstance().getTeleportManager().teleport((Player) sender, warp);
                 return false;
             }
         });
