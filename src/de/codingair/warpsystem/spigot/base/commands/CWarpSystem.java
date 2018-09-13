3a80017336f444622d2751f0b5bde5823a036185
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2018-09-13 13:45:54 +0200

    [WS-175] Add a list command for shortcuts

--- src/de/codingair/warpsystem/spigot/base/commands/CWarpSystem.java
+++ src/de/codingair/warpsystem/spigot/base/commands/CWarpSystem.java
@@ -26,6 +26,9 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class CWarpSystem extends CommandBuilder {
@@ -59,7 +62,7 @@ public class CWarpSystem extends CommandBuilder {
         getBaseComponent().addChild(new CommandComponent("shortcut") {
             @Override
             public boolean runCommand(CommandSender sender, String label, String[] args) {
-                sender.sendMessage("§8» §7" + Lang.get("Use", new Example("ENG", "Use"), new Example("GER", "Benutze")) + ": /" + label + " shortcut §e<add, remove>");
+                sender.sendMessage("§8» §7" + Lang.get("Use", new Example("ENG", "Use"), new Example("GER", "Benutze")) + ": /" + label + " shortcut §e<add, remove, list>");
                 return false;
             }
         }.setOnlyPlayers(true));
@@ -153,6 +156,37 @@ public class CWarpSystem extends CommandBuilder {
                 return false;
             }
         });
+
+        getComponent("shortcut").addChild(new CommandComponent("list") {
+            @Override
+            public boolean runCommand(CommandSender sender, String label, String[] args) {
+                List<String> message = new ArrayList<>();
+
+                if(ShortcutManager.getInstance().getShortcuts().isEmpty()) {
+                    message.add(" ");
+                    message.add("  §3§lShortcuts: §c-");
+                    message.add(" ");
+
+                    sender.sendMessage(message.toArray(new String[0]));
+                    return false;
+                }
+
+                for(Shortcut shortcut : ShortcutManager.getInstance().getShortcuts()) {
+                    message.add("  §7(" + (shortcut.getWarp() == null ? "Global warp" : "Warp") + ") §b" + (shortcut.getWarp() == null ? shortcut.getGlobalWarp() : shortcut.getWarp().getNameWithoutColor()) + " §7« \"§e" + shortcut.getDisplayName() + "§7\"");
+                }
+
+                Collections.sort(message);
+
+                message.add(0, " ");
+                message.add(1, " ");
+                message.add(2, "  §3§l§nShortcuts");
+                message.add(3, " ");
+                message.add(" ");
+
+                sender.sendMessage(message.toArray(new String[0]));
+                return false;
+            }
+        }.setOnlyPlayers(true));
 
 
         getBaseComponent().addChild(new CommandComponent("info") {
