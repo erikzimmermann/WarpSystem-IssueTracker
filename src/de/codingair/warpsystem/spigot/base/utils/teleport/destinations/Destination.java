0b80ec5d3518fb5397cce36b7108b89a6c67ad57
CodingAir <erik.zimmermann2000@yahoo.de>
2019-07-02 13:13:29 +0200

    [WS-117] Move to next release

--- src/de/codingair/warpsystem/spigot/base/utils/teleport/destinations/Destination.java
+++ src/de/codingair/warpsystem/spigot/base/utils/teleport/destinations/Destination.java
@@ -13,6 +13,8 @@ public class Destination {
     private String id;
     private DestinationType type;
     private DestinationAdapter adapter;
+    private double offsetX, offsetY, offsetZ;
+    private int signedX, signedY, signedZ;
 
     public Destination() {
         id = null;
@@ -44,6 +46,14 @@ public class Destination {
             this.type = json.get(0) == null ? null : DestinationType.valueOf((String) json.get(0));
             this.id = json.get(1) == null ? null : (String) json.get(1);
             this.adapter = type == null ? null : type.getInstance();
+            if(json.size() > 2) {
+                offsetX = Double.parseDouble(json.get(2) + "");
+                offsetY = Double.parseDouble(json.get(3) + "");
+                offsetZ = Double.parseDouble(json.get(4) + "");
+                signedX = Integer.parseInt(json.get(5) + "");
+                signedY = Integer.parseInt(json.get(6) + "");
+                signedZ = Integer.parseInt(json.get(7) + "");
+            }
         } catch(Exception ex) {
             throw new IllegalArgumentException("Wrong serialized data!", ex);
         }
@@ -54,12 +64,24 @@ public class Destination {
             this.id = null;
             this.adapter = null;
             this.type = null;
+            this.offsetX = 0;
+            this.offsetY = 0;
+            this.offsetZ = 0;
+            this.signedX = 0;
+            this.signedY = 0;
+            this.signedZ = 0;
             return this;
         }
 
         this.id = destination.id;
         this.adapter = destination.adapter;
         this.type = destination.type;
+        this.offsetX = destination.offsetX;
+        this.offsetY = destination.offsetY;
+        this.offsetZ = destination.offsetZ;
+        this.signedX = destination.signedX;
+        this.signedY = destination.signedY;
+        this.signedZ = destination.signedZ;
         return this;
     }
 
@@ -70,7 +92,13 @@ public class Destination {
     }
 
     public Location buildLocation() {
-        return adapter.buildLocation(id);
+        if(offsetX != 0 || offsetY != 0 || offsetZ != 0) {
+            double offsetX = signedX == 1 ? Math.random() * this.offsetX : signedX == -1 ? Math.random() * -this.offsetX : Math.random() * 2 * this.offsetX - this.offsetX;
+            double offsetY = signedY == 1 ? Math.random() * this.offsetY : signedY == -1 ? Math.random() * -this.offsetY : Math.random() * 2 * this.offsetY - this.offsetY;
+            double offsetZ = signedZ == 1 ? Math.random() * this.offsetZ : signedZ == -1 ? Math.random() * -this.offsetZ : Math.random() * 2 * this.offsetZ - this.offsetZ;
+
+            return adapter.buildLocation(id).add(offsetX, offsetY, offsetZ);
+        } else return adapter.buildLocation(id);
     }
 
     public double getCosts() {
@@ -112,6 +140,14 @@ public class Destination {
         JSONArray json = new JSONArray();
         json.add(type == null ? null : type.name());
         json.add(id);
+        if(offsetX != 0 || offsetY != 0 || offsetZ != 0) {
+            json.add(offsetX);
+            json.add(offsetY);
+            json.add(offsetZ);
+            json.add(signedX);
+            json.add(signedY);
+            json.add(signedZ);
+        }
 
         return json.toJSONString();
     }
@@ -122,8 +158,14 @@ public class Destination {
         if(o == null || getClass() != o.getClass()) return false;
         Destination that = (Destination) o;
         if(id == null) return that.id == null && type == that.type;
-        return id.equals(that.id) &&
-                type == that.type;
+        return id.equals(that.id)
+                && type == that.type
+                && offsetX == that.offsetX
+                && offsetY == that.offsetY
+                && offsetZ == that.offsetZ
+                && signedX == that.signedX
+                && signedY == that.signedY
+                && signedZ == that.signedZ;
     }
 
     public Destination clone() {
@@ -131,11 +173,65 @@ public class Destination {
         destination.id = id;
         destination.type = type;
         destination.adapter = adapter;
+        destination.offsetX = offsetX;
+        destination.offsetY = offsetY;
+        destination.offsetZ = offsetZ;
+        destination.signedX = signedX;
+        destination.signedY = signedY;
+        destination.signedZ = signedZ;
         return destination;
     }
 
     @Override
     public String toString() {
         return "Destination{id=" + this.id + ", " + this.type + "}";
+    }
+
+    public double getOffsetX() {
+        return offsetX;
+    }
+
+    public void setOffsetX(double offsetX) {
+        this.offsetX = offsetX;
+    }
+
+    public double getOffsetY() {
+        return offsetY;
+    }
+
+    public void setOffsetY(double offsetY) {
+        this.offsetY = offsetY;
+    }
+
+    public double getOffsetZ() {
+        return offsetZ;
+    }
+
+    public void setOffsetZ(double offsetZ) {
+        this.offsetZ = offsetZ;
+    }
+
+    public int getSignedX() {
+        return signedX;
+    }
+
+    public void setSignedX(int signedX) {
+        this.signedX = signedX;
+    }
+
+    public int getSignedY() {
+        return signedY;
+    }
+
+    public void setSignedY(int signedY) {
+        this.signedY = signedY;
+    }
+
+    public int getSignedZ() {
+        return signedZ;
+    }
+
+    public void setSignedZ(int signedZ) {
+        this.signedZ = signedZ;
     }
 }
