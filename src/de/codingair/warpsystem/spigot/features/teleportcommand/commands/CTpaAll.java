9ef5b936e473231e7163be262fea2bfb06e7b142
CodingAir <erik.zimmermann2000@yahoo.de>
2019-09-19 12:48:32 +0200

    [WS-158] Fixes the /tpaAll command for spigot

--- src/de/codingair/warpsystem/spigot/features/teleportcommand/commands/CTpaAll.java
+++ src/de/codingair/warpsystem/spigot/features/teleportcommand/commands/CTpaAll.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class CTpaAll extends CommandBuilder {
     public CTpaAll() {
-        super("TpAll", "A WarpSystem-Command", new BaseComponent(WarpSystem.PERMISSION_USE_TELEPORT_COMMAND_TPA_ALL) {
+        super("TpaAll", "A WarpSystem-Command", new BaseComponent(WarpSystem.PERMISSION_USE_TELEPORT_COMMAND_TPA_ALL) {
             @Override
             public void noPermission(CommandSender sender, String label, CommandComponent child) {
                 sender.sendMessage(Lang.getPrefix() + Lang.get("No_Permission"));
