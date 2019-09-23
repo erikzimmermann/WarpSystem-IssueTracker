421e1696bbcf49d1cab6776095138fee91d5d286
CodingAir <erik.zimmermann2000@yahoo.de>
2019-09-23 15:43:52 +0200

    [WS-164] Fixes the commands of global warps

--- src/de/codingair/warpsystem/spigot/features/globalwarps/commands/CGlobalWarp.java
+++ src/de/codingair/warpsystem/spigot/features/globalwarps/commands/CGlobalWarp.java
@@ -6,6 +6,7 @@ import de.codingair.codingapi.server.commands.builder.CommandComponent;
 import de.codingair.codingapi.server.commands.builder.MultiCommandComponent;
 import de.codingair.warpsystem.spigot.base.WarpSystem;
 import de.codingair.warpsystem.spigot.base.language.Lang;
+import de.codingair.warpsystem.spigot.base.utils.BungeeFeature;
 import de.codingair.warpsystem.spigot.base.utils.teleport.Origin;
 import de.codingair.warpsystem.spigot.base.utils.teleport.destinations.Destination;
 import de.codingair.warpsystem.spigot.base.utils.teleport.destinations.DestinationType;
@@ -15,9 +16,9 @@ import org.bukkit.entity.Player;
 
 import java.util.List;
 
-public class CGlobalWarp extends CommandBuilder {
+public class CGlobalWarp extends CommandBuilder implements BungeeFeature {
     public CGlobalWarp() {
-        super("GlobalWarp", "A WarpSystem-Command", new BaseComponent(WarpSystem.PERMISSION_USE_GLOBAL_WARPS) {
+        super("globalwarp", "A WarpSystem-Command", new BaseComponent(WarpSystem.PERMISSION_USE_GLOBAL_WARPS) {
             @Override
             public void noPermission(CommandSender sender, String label, CommandComponent child) {
                 sender.sendMessage(Lang.getPrefix() + Lang.get("No_Permission"));
@@ -35,11 +36,17 @@ public class CGlobalWarp extends CommandBuilder {
 
             @Override
             public boolean runCommand(CommandSender sender, String label, String[] args) {
-                sender.sendMessage(Lang.getPrefix() + "§7" + Lang.get("Use") + ": /" + label + " §e<warp>");
+                if(WarpSystem.getInstance().isOnBungeeCord()) sender.sendMessage(Lang.getPrefix() + "§7" + Lang.get("Use") + ": /" + label + " §e<warp>");
+                else sender.sendMessage(Lang.getPrefix() + Lang.get("Connect_BungeeCord"));
                 return false;
             }
         }, true, "gw", "gwarp");
 
+        WarpSystem.getInstance().getBungeeFeatureList().add(this);
+    }
+
+    @Override
+    public void onConnect() {
         getBaseComponent().addChild(new MultiCommandComponent() {
             @Override
             public void addArguments(CommandSender sender, String[] args, List<String> suggestions) {
@@ -48,10 +55,19 @@ public class CGlobalWarp extends CommandBuilder {
 
             @Override
             public boolean runCommand(CommandSender sender, String label, String argument, String[] args) {
-                WarpSystem.getInstance().getTeleportManager().teleport((Player) sender, Origin.GlobalWarp, new Destination(argument, DestinationType.GlobalWarp), argument, 0,
-                        WarpSystem.getInstance().getFileManager().getFile("Config").getConfig().getBoolean("WarpSystem.Send.Teleport_Message.GlobalWarps", true));
+                if(GlobalWarpManager.getInstance().exists(argument)) {
+                    WarpSystem.getInstance().getTeleportManager().teleport((Player) sender, Origin.GlobalWarp, new Destination(argument, DestinationType.GlobalWarp), argument, 0,
+                            WarpSystem.getInstance().getFileManager().getFile("Config").getConfig().getBoolean("WarpSystem.Send.Teleport_Message.GlobalWarps", true));
+                } else {
+                    sender.sendMessage(Lang.getPrefix() + Lang.get("WARP_DOES_NOT_EXISTS"));
+                }
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onDisconnect() {
+        getBaseComponent().removeChild((String) null);
     }
 }
