8b0c07ef25cc47824cda5d4aea60a6f5dab361f9
CodingAir <erik.zimmermann2000@yahoo.de>
2020-04-09 17:12:18 +0200

    [WS-242] Improves /back * Deaths will only be recognized as back position if a player has the permission 'WarpSystem.Use.TeleportCommand.Back.Deaths' * /back works unlimited times now if the history size is set to 1

--- src/de/codingair/warpsystem/spigot/features/teleportcommand/listeners/BackListener.java
+++ src/de/codingair/warpsystem/spigot/features/teleportcommand/listeners/BackListener.java
@@ -1,11 +1,15 @@
 package de.codingair.warpsystem.spigot.features.teleportcommand.listeners;
 
+import de.codingair.warpsystem.spigot.base.WarpSystem;
 import de.codingair.warpsystem.spigot.features.teleportcommand.TeleportCommandManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.RegisteredListener;
 
 public class BackListener implements Listener {
 
@@ -16,12 +20,16 @@ public class BackListener implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
-        TeleportCommandManager.getInstance().addToBackHistory(e.getEntity(), e.getEntity().getLocation());
+        if(WarpSystem.hasPermission(e.getEntity(), WarpSystem.PERMISSION_USE_TELEPORT_COMMAND_BACK_DETECT_DEATHS))
+            TeleportCommandManager.getInstance().addToBackHistory(e.getEntity(), e.getEntity().getLocation());
     }
 
     @EventHandler
     public void onTeleport(PlayerTeleportEvent e) {
-        if(e.getCause() == PlayerTeleportEvent.TeleportCause.UNKNOWN || TeleportCommandManager.getInstance().usingBackCommand(e.getPlayer())) return;
+        if(!WarpSystem.hasPermission(e.getPlayer(), WarpSystem.PERMISSION_USE_TELEPORT_COMMAND_BACK)) return;
+
+        if(e.getCause() != PlayerTeleportEvent.TeleportCause.PLUGIN && e.getCause() != PlayerTeleportEvent.TeleportCause.COMMAND) return;
+        if(TeleportCommandManager.getInstance().getBackHistorySize() > 1 && TeleportCommandManager.getInstance().usingBackCommand(e.getPlayer())) return;
         TeleportCommandManager.getInstance().addToBackHistory(e.getPlayer(), e.getFrom());
     }
 
